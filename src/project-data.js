7c071e03e438a7a7cd5449328a2ff7950957b36d
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-19 14:28:49 -0400

    creating the data file for the projects in order to loop over the data in the projects component

---
+++ src/project-data.js
@@ -0,0 +1,9 @@
+const projects = {
+  project1: {
+    name: 'James Dean Does Other Stuff',
+    image: '/images/James_Hmpage.png',
+    desc: 'Here is a website showcasing the artwork of a friend of mine.',
+    liveUrl: 'https://james-photo-gallery-garrettanderson.netlify.com/',
+    githubUrl: 'https://github.com/GarrettAnderson/james-photo-gallery'
+  }
+}
