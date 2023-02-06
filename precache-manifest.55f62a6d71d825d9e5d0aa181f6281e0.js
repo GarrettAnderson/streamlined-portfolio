b160296264b15ee319f13f61bb2977c26454a9a1
Garrett <garrettleegrahamanderson@gmail.com>
2023-02-06 16:22:19 -0500

    Updates

---
+++ precache-manifest.55f62a6d71d825d9e5d0aa181f6281e0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e043c2ab988bffd73423e98cf31bd07",
+    "url": "/streamlined-portfolio/index.html"
+  },
+  {
+    "revision": "0322fe7d5a3fcf46e483",
+    "url": "/streamlined-portfolio/static/css/main.ccc5fcaf.chunk.css"
+  },
+  {
+    "revision": "e1d233c0b2b68c1f56a3",
+    "url": "/streamlined-portfolio/static/js/2.c0fc56ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/streamlined-portfolio/static/js/2.c0fc56ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0322fe7d5a3fcf46e483",
+    "url": "/streamlined-portfolio/static/js/main.f45fedba.chunk.js"
+  },
+  {
+    "revision": "ad836e8b8a8cbbbce42b",
+    "url": "/streamlined-portfolio/static/js/runtime-main.e557e187.js"
+  },
+  {
+    "revision": "b38c29f8d4230b6027e61a3ef81cb3fa",
+    "url": "/streamlined-portfolio/static/media/ArtDesiScreenshot.b38c29f8.png"
+  },
+  {
+    "revision": "61ac48b5ccc42f86dff8fd3b2b1ced9f",
+    "url": "/streamlined-portfolio/static/media/CyborgScreenshot.61ac48b5.png"
+  },
+  {
+    "revision": "7a819883d18de821dae3ab6478d3e85f",
+    "url": "/streamlined-portfolio/static/media/DiceGameScreenshot.7a819883.png"
+  },
+  {
+    "revision": "f1a0e01e8b3aea7ba5512522a8131f58",
+    "url": "/streamlined-portfolio/static/media/ErinWeddingScreenshot.f1a0e01e.png"
+  },
+  {
+    "revision": "b0e038af21168ef45a3b50bef716bdf3",
+    "url": "/streamlined-portfolio/static/media/JamesDeanScreenshot.b0e038af.png"
+  },
+  {
+    "revision": "b7d6f093c97376abd9fc8263130427f8",
+    "url": "/streamlined-portfolio/static/media/James_Hmpage.b7d6f093.png"
+  },
+  {
+    "revision": "c1a55d845dc550cd7e27547ef37abad1",
+    "url": "/streamlined-portfolio/static/media/James_Screenshot.c1a55d84.png"
+  },
+  {
+    "revision": "3c98ef64ca0f31e949864fd1981cd088",
+    "url": "/streamlined-portfolio/static/media/MonsterRolodexScreenshot.3c98ef64.png"
+  },
+  {
+    "revision": "e345e883c70f092020e2c716c20ac7fe",
+    "url": "/streamlined-portfolio/static/media/ajaxAhead.e345e883.png"
+  },
+  {
+    "revision": "ea185e418c5a28a563915619626aa632",
+    "url": "/streamlined-portfolio/static/media/bombSnifferImg.ea185e41.png"
+  },
+  {
+    "revision": "3ab8df4df67bfcbb5e8152d5387f7f91",
+    "url": "/streamlined-portfolio/static/media/catchOfTheDay.3ab8df4d.png"
+  },
+  {
+    "revision": "c2130ab13843c29e16d09e42006237b2",
+    "url": "/streamlined-portfolio/static/media/colorPickerImg.c2130ab1.png"
+  },
+  {
+    "revision": "8b52586afbbbcc5e8822cd1b52bad7cd",
+    "url": "/streamlined-portfolio/static/media/cropHeadShot.8b52586a.png"
+  },
+  {
+    "revision": "93dc9abc93a34db19703c40a50a34152",
+    "url": "/streamlined-portfolio/static/media/flexCSSPanel.93dc9abc.png"
+  },
+  {
+    "revision": "8c6213f61e142381122639188afdfca5",
+    "url": "/streamlined-portfolio/static/media/screencapture-simple-react-invoice.8c6213f6.png"
+  },
+  {
+    "revision": "697069b51f62b9033136599b41fd2f2e",
+    "url": "/streamlined-portfolio/static/media/toTheMoviesImg.697069b5.png"
+  },
+  {
+    "revision": "126c0dbac7e481cd5e7c842165eff1df",
+    "url": "/streamlined-portfolio/static/media/yogiMeImg.126c0dba.png"
+  }
+]);
